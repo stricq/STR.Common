e6286722c3fb229cc9b31b6c2795e9fbd97246ab
stricq <stricq@stricq.com>
2019-04-18 23:04:02 -0600

    Split FileDownloadProgressMessage class.

--- Str.Common/Messages/FileDownloadProgressMessage.cs
+++ Str.Common/Messages/FileDownloadProgressMessage.cs
@@ -2,13 +2,17 @@
 
 namespace Str.Common.Messages {
 
-  public class FileDownloadProgressMessage<T> : MessageBase {
+  public class FileDownloadProgressMessage : MessageBase {
 
     public long BytesCurrent { get; set; }
 
     public long BytesTotal { get; set; }
 
     public bool IsComplete { get; set; }
+
+  }
+
+  public class FileDownloadProgressMessage<T> : FileDownloadProgressMessage {
 
     public T State { get; set; }
 

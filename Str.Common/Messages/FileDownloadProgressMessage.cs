a926ecc7ccc3e4bf4efc6ee03f1b61f2a43bfa01
stricq <stricq@stricq.com>
2019-04-24 19:37:52 -0600

    Added PercentComplete to FileDownloadProgressMessage.

--- Str.Common/Messages/FileDownloadProgressMessage.cs
+++ Str.Common/Messages/FileDownloadProgressMessage.cs
@@ -1,7 +1,11 @@
-﻿
+﻿using System.Diagnostics.CodeAnalysis;
+
 
 namespace Str.Common.Messages {
 
+  [SuppressMessage("ReSharper", "UnusedMember.Global")]
+  [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+  [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
   public class FileDownloadProgressMessage : MessageBase {
 
     public long BytesCurrent { get; set; }
@@ -10,8 +14,12 @@ namespace Str.Common.Messages {
 
     public bool IsComplete { get; set; }
 
+    public double PercentComplete => BytesTotal == 0 ? 0 : (double)BytesCurrent / BytesTotal;
+
   }
 
+  [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+  [SuppressMessage("ReSharper", "UnusedMember.Global")]
   public class FileDownloadProgressMessage<T> : FileDownloadProgressMessage {
 
     public T State { get; set; }

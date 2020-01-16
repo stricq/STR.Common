5848603fd483e583179b4cf6b4b5e7552f95a28c
stricq <stricq@stricq.com>
2020-01-15 21:05:47 -0700

    Fix some async issues.

--- Str.Common/Extensions/HttpContentExtensions.cs
+++ Str.Common/Extensions/HttpContentExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
@@ -7,10 +8,12 @@ using System.Threading.Tasks;
 
 namespace Str.Common.Extensions {
 
+  [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "This is a library.")]
+  [SuppressMessage("ReSharper", "UnusedType.Global", Justification = "This is a library.")]
   public static class HttpContentExtensions {
 
     public static async Task<Stream> GetResponseStreamWithDecompressionAsync(this HttpContent response) {
-      Stream responseStream = await response.ReadAsStreamAsync();
+      Stream responseStream = await response.ReadAsStreamAsync().Fire();
 
       if (response.Headers.ContentEncoding.Any(h => h.ToLower() == "gzip")) responseStream = new GZipStream(responseStream, CompressionMode.Decompress);
       else if (response.Headers.ContentEncoding.Any(h => h.ToLower() == "deflate")) responseStream = new DeflateStream(responseStream, CompressionMode.Decompress);
